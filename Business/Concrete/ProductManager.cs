6c27457b064208697447d88e866d2d17533088e9
Tarık Hamarat <trkhamarat@gmail.com>
2022-04-26 00:47:16 +0300

    Practice

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {   //Dependency injection yaptık
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {   //Bu şekilde IProductdal içindeki getAll metoduna ulaşabildik.
+            return _productDal.GetAll();
+        }
+    }
+}
